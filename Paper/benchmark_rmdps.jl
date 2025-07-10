7216b5a1e9507fa5b7dac57440b698993740aba9
Keith Badger <keithbadger699@gmail.com>
2025-07-09 21:15:28 -0400

    seperated game and rmdp benchmarks

---
+++ Paper/benchmark_rmdps.jl
@@ -0,0 +1,13 @@
+include("rmdp_algorithm_comparison.jl")
+
+using DataFrames, Arrow
+
+function benchmark_rmdps()
+    algorithms = [(name = "PAI",), (name = "KB",), (name = "RCPI",), (name = "FT", η = 1e-3, β = .5), (name = "VI",)]
+    output_inventory = benchmark_run_inventory(algorithms, #= repeat(40:40:200, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,Inf)
+    Arrow.write("Paper/data/inventory_large.arrow", output_inventory)
+    output_ruin = benchmark_run_ruin(algorithms, #= repeat(20:20:100, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,Inf)
+    Arrow.write("Paper/data/ruin_large.arrow", output_ruin)
+    output_grid = benchmark_run_gridworld(algorithms, #= repeat(4:4:20, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,Inf)
+    Arrow.write("Paper/data/gridworld_large.arrow", output_grid)
+end
