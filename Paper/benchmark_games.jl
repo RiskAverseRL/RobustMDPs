a1f10c7e037bd5a1f1162ad359797b912d49ad26
Keith Badger <keithbadger699@gmail.com>
2025-07-09 22:22:47 -0400

    time limits

--- Paper/benchmark_games.jl
+++ Paper/benchmark_games.jl
@@ -5,6 +5,6 @@ using DataFrames, Arrow
 
 
 algorithms = [(name = "PAI",), (name = "KB",), (name = "RCPI",), (name = "FT", η = 1e-3, β = .5), (name = "VI",)] 
-output_games = benchmark_run_games(#= repeat(200:200:1000, inner = 20) =# [3], [.9 ,.99, .999], algorithms, [1,2,3,5,10], -10., 10., .2, 1e-3, 259200.)
+output_games = benchmark_run_games(repeat(200:200:1000, inner = 20), [.9 ,.99, .999], algorithms, [1,2,3,5,10], -10., 10., .2, 1e-3, 28800.)
 Arrow.write("Paper/data/games_large.arrow", output_games)
 
--- Paper/benchmark_rmdps.jl
+++ Paper/benchmark_rmdps.jl
@@ -3,9 +3,9 @@ include("rmdp_algorithm_comparison.jl")
 using DataFrames, Arrow
 
 algorithms = [(name = "PAI",), (name = "KB",), (name = "RCPI",), (name = "FT", η = 1e-3, β = .5), (name = "VI",)]
-output_inventory = benchmark_run_inventory(algorithms, #= repeat(40:40:200, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,259200.)
+output_inventory = benchmark_run_inventory(algorithms, repeat(40:40:200, inner = 20), [.9 ,.99, .999],1,1e-3,28800.)
 Arrow.write("Paper/data/inventory_large.arrow", output_inventory)
-output_ruin = benchmark_run_ruin(algorithms, #= repeat(20:20:100, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,259200.)
+output_ruin = benchmark_run_ruin(algorithms, repeat(20:20:100, inner = 20), [.9 ,.99, .999],1,1e-3,28800.)
 Arrow.write("Paper/data/ruin_large.arrow", output_ruin)
-output_grid = benchmark_run_gridworld(algorithms, #= repeat(4:4:20, inner = 20) =# [3], [.9 ,.99, .999],1,1e-3,259200.)
+output_grid = benchmark_run_gridworld(algorithms, repeat(4:4:20, inner = 20), [.9 ,.99, .999],1,1e-3,28800.)
 Arrow.write("Paper/data/gridworld_large.arrow", output_grid)
